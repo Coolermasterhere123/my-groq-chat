f4adedecb581f79e6d509d1ddee1987fa072f7cf
Wes <coolermaster123@protonmail.com>
2026-04-11 12:48:27 -0700

    Add all project files

---
+++ next.cfg.mjs
@@ -0,0 +1,8 @@
+// next.config.mjs – default config is fine for this demo
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default nextConfig;
